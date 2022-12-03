c1b8f9c70379087e4109d5ff2445c5bf581b4fd7
willharvv <112087181+willharvv@users.noreply.github.com>
2022-12-03 22:36:01 +0000

    W

---
+++ learn.js
@@ -0,0 +1,74 @@
+  let value = prompt ("Password");
+
+    if (value == 'secret' ) 
+    alert ('you may pass');
+
+    else 
+    alert ('stay where you are' );
+
+
+    let value = prompt ('how old are you');
+
+    if (value < 18 )
+    alert ('to young');
+
+    else 
+    alert ('your in the right place');
+
+
+    let log = prompt ('login:');
+
+    let alert = ('employee') ? 'hello';
+    ('director') ? 'greetings';
+     'no login';
+
+     let str = 'apple,banana,kiwi';
+     let part = str.slice(7,13)
+
+
+     let a = 5;
+     let b = 9;
+
+     alert (5*9);
+/////////////////////////////////////
+     let value = prompt ('password?');
+
+     if (value == 'Password1')
+     alert('You may pass');
+
+     else 
+     alert ('you may go no further');
+//////////////////////////////////////
+     let value = prompt ("Password");
+
+     if (value == 'secret' ) 
+     alert ('you may pass');
+ 
+     else 
+     alert ('stay where you are' );
+     /////////////////////////////////
+
+     
+     alert ('good morning');
+
+     let email = prompt ('email');
+       
+     if (email == '@gmail')
+      alert('good');
+
+      else
+      alert ('try and start agian');
+
+
+     let value = prompt ('password?');
+         
+        if (value == 'please')
+         alert ('your in');
+
+         else 
+         alert ('your locked out');
+
+
+
+
+         getting this shit 
